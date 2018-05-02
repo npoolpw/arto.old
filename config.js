2c599d502a7310983bd1c23071c7e709bee2ff25
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-05-02 17:39:38 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,26 @@
+var api = "http://sg1.npool.pw:3000";
+
+var api_blockexplorer = "http://explorer.arto.cash:19451";
+
+var poolHost = "sg1.npool.pw";
+
+var irc = "";
+
+var email = "dev@arto.cash";
+
+var cryptonatorWidget = ["BTC-USD"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://explorer.arto.cash/?hash={id}#blockchain_block";
+
+var transactionExplorer = "http://explorer.arto.cash/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default-theme.css";
+
+var networkStat = {
+    "rto": [
+		["pool.arto.cash", "http://pool.arto.cash:8117"],
+		["npool.pw/arto", "http://sg1.npool.pw:3000"]
+    ]
+};
